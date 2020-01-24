6e1ab4c08a830949dcddcbfb0be7edabe08f56bb
Rodrigo Fernández <rodrigofernandez@MacBook-Pro-de-Rodrigo.local>
2020-01-24 14:45:46 +0100

    nuevo menú

--- project/start.js
+++ project/start.js
@@ -17,6 +17,10 @@ app.get('/about', function(request, response) {
 	response.render('pages/about');
 });
 
+app.get('/proyect', function(request, response) {
+	response.render('pages/proyect');
+});
+
 app.listen(app.get('port'), function() {
 	console.log('Node app is running on port', app.get('port'));
 });
