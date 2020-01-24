44a3898588e0fc43f52c7f6930082e033233cbd6
Rodrigo Fernández <rodrigofernandez@MacBook-Pro-de-Rodrigo.local>
2020-01-24 14:34:56 +0100

    heroku local no funciona

--- project/start.js
+++ project/start.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', process.env.PORT || 5000);
 
 app.use(express.static(__dirname));
 
@@ -10,13 +10,16 @@ app.set('views', __dirname + '/html');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+	response.render('pages/index');
+});
+
+app.get('/about', function(request, response) {
+	response.render('pages/about');
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+	console.log('Node app is running on port', app.get('port'));
 });
-
 
 // This file is what handles incoming requests and
 // serves files to the browser, or executes server-side code
